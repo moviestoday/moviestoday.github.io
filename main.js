aa5da0775a5b0d761486ca2e340b69f16574cf37
Bishaka Samuel <sbishaka@gmail.com>
2016-03-25 14:41:32 +0300

    Make init commit of Movies Today 2016 Site

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Bishaka on 25/03/2016.
+ */
+
+var
+    apiHost = "http://bt-njsmoviestoday.rhcloud.com";
+    moviestoday = angular.module("moviestoday",[])
+    .controller("mainCtrl",["$scope","$http",function($scope,$http){
+        console.log("We up and running");
+            $scope.loadingCinemas = true;
+            $http.get(apiHost+"/api/v2/all").success(function(data){
+                $scope.loadingCinemas = false;
+                $scope.cinemas = data;
+                console.log(data);
+            })
+    }])
+;
